7177b50b76e93bd529e1a29f8c545d9cdc3c1304
Jonas Lobe <jonas.lobe.2006@outlook.com>
2021-11-13 10:56:49 +0100

    simplify drawing on the gui

--- src/main/java/de/jonas/teacherrating/gui/StartGui.java
+++ src/main/java/de/jonas/teacherrating/gui/StartGui.java
@@ -1,6 +1,11 @@
 package de.jonas.teacherrating.gui;
 
 import de.jonas.teacherrating.object.Gui;
+import org.jetbrains.annotations.NotNull;
+
+import java.awt.Graphics;
+
+import static java.awt.Color.LIGHT_GRAY;
 
 /**
  * Ein {@link StartGui} stammt von einem {@link Gui} ab und es stellt das {@link Gui} dar, welches als erstes geöffnet
@@ -8,18 +13,36 @@ import de.jonas.teacherrating.object.Gui;
  */
 public final class StartGui extends Gui {
 
+    //<editor-fold desc="CONSTANTS">
     /** Der Titel des Fensters. */
     private static final String TITLE = "Sammle Daten...";
     /** Die Breite des Fensters. */
     private static final int WIDTH = 750;
-    /** DIe Höhe des Fensters. */
+    /** Die Höhe des Fensters. */
     private static final int HEIGHT = 500;
+    //</editor-fold>
 
+
+    //<editor-fold desc="CONSTRUCTORS">
 
     /**
      * Erzeugt eine neue Instanz eines {@link StartGui}, welches von einem {@link Gui} abstammt.
      */
     public StartGui() {
-        super(TITLE, WIDTH, HEIGHT);
+        super(
+            TITLE,
+            WIDTH,
+            HEIGHT,
+            LIGHT_GRAY
+        );
     }
+    //</editor-fold>
+
+
+    //<editor-fold desc="implementation">
+    @Override
+    public void draw(@NotNull final Graphics g) {
+
+    }
+    //</editor-fold>
 }
--- src/main/java/de/jonas/teacherrating/object/Gui.java
+++ src/main/java/de/jonas/teacherrating/object/Gui.java
@@ -1,15 +1,29 @@
 package de.jonas.teacherrating.object;
 
+import lombok.Getter;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Range;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 
 /**
  * Ein {@link Gui} stellt ein Objekt für ein Fenster dar, welches vererbt werden kann und für eine vereinfachte
  * Instanziierung eines {@link JFrame Fensters} genutzt werden kann.
  */
-public class Gui extends JFrame {
+public abstract class Gui extends JFrame {
+
+    //<editor-fold desc="LOCAL FIELDS">
+    /** Das Zeichen-Objekt, mit dem alles nötige auf dieses {@link Gui} gezeichnet wird. */
+    @Getter
+    private final Draw draw;
+    //</editor-fold>
+
 
     //<editor-fold desc="CONSTRUCTORS">
 
@@ -18,14 +32,16 @@ public class Gui extends JFrame {
      * ein Fenster dar, welches vererbt werden kann und für eine vereinfachte Instanziierung eines {@link JFrame
      * Fensters} genutzt werden kann.
      *
-     * @param title  Der Titel, welcher das Fenster tragen soll.
-     * @param width  Die Breite, die das Fenster haben soll.
-     * @param height Die Höhe, die das Fenster haben soll.
+     * @param title      Der Titel, welcher das Fenster tragen soll.
+     * @param width      Die Breite, die das Fenster haben soll.
+     * @param height     Die Höhe, die das Fenster haben soll.
+     * @param background Die Hintergrund-Farbe für das Fenster.
      */
     public Gui(
         @NotNull final String title,
         @Range(from = 0, to = Integer.MAX_VALUE) final int width,
-        @Range(from = 0, to = Integer.MAX_VALUE) final int height
+        @Range(from = 0, to = Integer.MAX_VALUE) final int height,
+        @NotNull final Color background
     ) {
         super(title);
 
@@ -33,6 +49,13 @@ public class Gui extends JFrame {
         super.setBounds(0, 0, width, height);
         super.setLocationRelativeTo(null);
         super.setLayout(null);
+        super.setResizable(false);
+
+        this.draw = new Draw(background);
+        this.draw.setBounds(0, 0, width, height);
+        this.draw.setVisible(true);
+
+        super.add(this.draw);
     }
     //</editor-fold>
 
@@ -50,5 +73,61 @@ public class Gui extends JFrame {
     public void close() {
         super.dispose();
     }
+
+    /**
+     * Zeichnet auf das {@link Gui}, mithilfe des {@link Graphics Graphics-Objekt}.
+     *
+     * @param g Die {@link Graphics}, mit denen auf das {@link Gui} gezeichnet werden.
+     */
+    public abstract void draw(@NotNull Graphics g);
+
+
+    //<editor-fold desc="Draw">
+
+    /**
+     * Die Klasse, mit dessen Hilfe alle Zeichnungen auf dem {@link Gui} vorgenommen werden.
+     */
+    private final class Draw extends JLabel {
+
+        //<editor-fold desc="LOCAL FIELDS">
+        /** Die Hintergrund-Farbe, die das {@link Gui} haben soll. */
+        private final Color background;
+        //</editor-fold>
+
+
+        //<editor-fold desc="CONSTRUCTORS">
+
+        /**
+         * Erzeugt eine neue Instanz eines {@link Draw}, mit dessen Hilfe alle Zeichnungen auf dem {@link Gui}
+         * vorgenommen werden.
+         *
+         * @param background Die Hintergrund-Farbe, die das {@link Gui} haben soll.
+         */
+        private Draw(@NotNull final Color background) {
+            this.background = background;
+        }
+        //</editor-fold>
+
+
+        //<editor-fold desc="implementation">
+        @Override
+        protected void paintComponent(@NotNull final Graphics g) {
+            super.paintComponent(g);
+
+            final Graphics2D g2d = (Graphics2D) g;
+
+            // define render quality
+            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+            // draw background
+            g.setColor(this.background);
+            g.fillRect(0, 0, this.getWidth(), this.getHeight());
+
+            g.setColor(Color.BLACK);
+            Gui.this.draw(g);
+        }
+        //</editor-fold>
+    }
+    //</editor-fold>
 
 }
